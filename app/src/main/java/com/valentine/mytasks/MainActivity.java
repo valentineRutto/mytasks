ac9cfc5876b8740c218d2945e28225525b0c3eaa
valentinerutto <rutto28@gmail.com>
2015-08-19 21:36:35 +0300

    initialized parse

--- app/src/main/java/com/valentine/mytasks/MainActivity.java
+++ app/src/main/java/com/valentine/mytasks/MainActivity.java
@@ -1,9 +1,11 @@
 package com.valentine.mytasks;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.parse.Parse;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.initialize(this, "APP_ID", "CLIENT_ID");
     }
 
     @Override
