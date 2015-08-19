d26a5eb1dd9cb97ee289e84584580a3341639aad
valentinerutto <rutto28@gmail.com>
2015-08-19 21:56:22 +0300

    cdreated is completed method

--- app/src/main/java/com/valentine/mytasks/Task.java
+++ app/src/main/java/com/valentine/mytasks/Task.java
@@ -1,11 +1,18 @@
 package com.valentine.mytasks;
 
 import com.parse.ParseClassName;
+import com.parse.ParseObject;
 
 /**
  * Created by valentine on 8/19/15.
  */
-@ParseClassName("Task")
-public class Task {
+@ParseClassName("Tasks")
+public class Task extends ParseObject{
+    public Task(){
+    }
+    public boolean isCompleted(){
+        return getBoolean("completed");
+    }
+
 
 }
