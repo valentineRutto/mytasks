64100131db4ddc37fc1c4b401f011a62d7c6435a
valentinerutto <rutto28@gmail.com>
2015-08-19 23:01:10 +0300

    removed some analytics due to some getintent error

--- app/src/main/java/com/valentine/mytasks/MyApp.java
+++ app/src/main/java/com/valentine/mytasks/MyApp.java
@@ -1,15 +1,17 @@
 package com.valentine.mytasks;
 
 import com.parse.Parse;
-import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
 
 /**
  * Created by valentine on 8/19/15.
  */
-public class MyApp {
+public class MyApp extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "bDrxYwWkcBY0kjqk8yh333FBXspyYUJDQIkvB7uf", "9Eh1b8QHjAczgfBHNcrzyy4sBXml6bocKd2TQ1W6");
+        ParseObject.registerSubclass(Task.class);
+    }
 
-    Parse.initialize(this, "bDrxYwWkcBY0kjqk8yh333FBXspyYUJDQIkvB7uf", "9Eh1b8QHjAczgfBHNcrzyy4sBXml6bocKd2TQ1W6");
-    ParseAnalytics.trackAppOpened(getIntent());
-    ParseObject.registerSubclass(Task.class);
 }
