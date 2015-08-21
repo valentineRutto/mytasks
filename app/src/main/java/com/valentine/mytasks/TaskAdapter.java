6c0face003128bddb7db2ab3084db8bba24ccd11
valentinerutto <rutto28@gmail.com>
2015-08-21 19:40:00 +0300

    created taskadapter for querying the tasks

--- app/src/main/java/com/valentine/mytasks/TaskAdapter.java
+++ app/src/main/java/com/valentine/mytasks/TaskAdapter.java
@@ -1,7 +1,14 @@
 package com.valentine.mytasks;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by valentine on 8/20/15.
  */
-public class TaskAdapter {
+public class TaskAdapter extends ArrayAdapter<Task> {
+
+    public TaskAdapter(Context context, int resource) {
+        super(context, resource);
+    }
 }
