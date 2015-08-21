323b9eb4dadf88d2f2c5948746d211493623bd3b
valentinerutto <rutto28@gmail.com>
2015-08-21 19:43:40 +0300

    set objectgs and context to taskrow item layout

--- app/src/main/java/com/valentine/mytasks/TaskAdapter.java
+++ app/src/main/java/com/valentine/mytasks/TaskAdapter.java
@@ -3,12 +3,19 @@ package com.valentine.mytasks;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
+import java.util.List;
+
 /**
  * Created by valentine on 8/20/15.
  */
 public class TaskAdapter extends ArrayAdapter<Task> {
+    private Context mContext;
+    private List<Task> mTasks;
 
     public TaskAdapter(Context context, int resource) {
-        super(context, resource);
+        super(context, R.layout.task_row_item, objects);
+        this.mContext = context;
+        this.mTasks = objects;
+
     }
 }
