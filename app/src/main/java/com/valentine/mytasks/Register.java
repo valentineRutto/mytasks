be03a1cdc12787ea569fc5f224221b66ccea085b
valentinerutto <rutto28@gmail.com>
2015-09-22 16:22:41 +0300

    created userregister funtion

--- app/src/main/java/com/valentine/mytasks/Register.java
+++ app/src/main/java/com/valentine/mytasks/Register.java
@@ -1,17 +1,48 @@
 package com.valentine.mytasks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class Register extends AppCompatActivity {
+mUSername
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+    public void register(final View v){
+        if(mUsernameField.getText().length() == 0 || mPasswordField.getText().length() == 0)
+            return;
+
+        v.setEnabled(false);
+        ParseUser user = new ParseUser();
+        user.setUsername(mUsernameField.getText().toString());
+        user.setPassword(mPasswordField.getText().toString());
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Intent intent = new Intent(Register.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                    v.setEnabled(true);
+                }
+            }
+        });
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
